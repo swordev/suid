638ba1878a2b316d9464903172e8a021a6ab7570
Juanra GM <juanrgm724@gmail.com>
2022-03-23 23:35:43 +0100

    fix(site): set correct docs link

--- packages/site/src/pages/components/IconsPage/IconsPage.tsx
+++ packages/site/src/pages/components/IconsPage/IconsPage.tsx
@@ -3,5 +3,7 @@ import ComponentInfo from "~/components/ComponentInfo";
 import Example from "./Example";
 
 export default function IconsPage() {
-  return <ComponentInfo name={Icon.name} examples={[Example]} />;
+  return (
+    <ComponentInfo name={Icon.name} examples={[Example]} docsName="icons" />
+  );
 }
