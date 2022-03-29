c898be23e88e60640459a3eb0e99c9957d8f29a6
Juanra GM <juanrgm724@gmail.com>
2022-03-30 00:55:23 +0200

    fix(material): remove global style already created by `CssBaseline`

--- packages/material/src/styles/ThemeProvider.tsx
+++ packages/material/src/styles/ThemeProvider.tsx
@@ -1,19 +1,11 @@
 import { Theme } from ".";
 import ThemeContext from "@suid/system/ThemeContext";
-import createSxClass from "@suid/system/createSxClass";
 import { JSXElement } from "solid-js";
 
 function ThemeProvider<T extends string>(props: {
   theme: Theme<T>;
   children: JSXElement;
 }) {
-  createSxClass(() => ({
-    "@global": {
-      body: {
-        backgroundColor: props.theme.palette.background.default,
-      },
-    },
-  }));
   return (
     <ThemeContext.Provider value={props.theme}>
       {props.children}
