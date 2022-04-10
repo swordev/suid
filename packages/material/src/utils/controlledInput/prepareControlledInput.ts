ea949c3ebee795cf1e21a2ae2b880cb6a4122624
Juanra GM <juanrgm724@gmail.com>
2022-04-11 01:43:12 +0200

    fix(material): drop input transforms due to incompatibilities with Input Events Level 2

--- packages/material/src/utils/controlledInput/prepareControlledInput.ts
+++ packages/material/src/utils/controlledInput/prepareControlledInput.ts
@@ -1,28 +1,21 @@
-import applyInputTransform from "./applyInputTransform";
-
 function prepareControlledInput(options: {
   element: HTMLInputElement | HTMLTextAreaElement;
-  onChange: (event: InputEvent, value: string, start: number) => void;
+  onChange: (event: Event, value: string, start: number) => void;
 }) {
-  (options.element as HTMLInputElement).addEventListener(
-    "beforeinput",
-    (event) => {
-      const input = event.currentTarget as HTMLInputElement;
-      if (event.inputType === "insertLineBreak") return;
-      if (typeof input.selectionStart !== "number")
-        throw new Error(`'selectionStart' can not be null`);
-      if (typeof input.selectionEnd !== "number")
-        throw new Error(`'selectionEnd' can not be null`);
-      const [value, start] = applyInputTransform(
-        input.value,
-        event.inputType,
-        event.data || "",
-        input.selectionStart,
-        input.selectionEnd
-      );
-      options.onChange(event, value, start);
-      event.preventDefault();
-    }
-  );
+  const input = options.element as HTMLInputElement;
+  let prevValue = "";
+  let prevSelectionStart = 0;
+  input.addEventListener("beforeinput", () => {
+    prevValue = input.value;
+    prevSelectionStart = input.selectionStart ?? prevValue.length;
+  });
+  input.addEventListener("input", (event) => {
+    const value = input.value;
+    const start = input.selectionStart ?? value.length;
+    input.value = prevValue;
+    input.selectionStart = input.selectionEnd = prevSelectionStart;
+    options.onChange(event, value, start);
+  });
 }
+
 export default prepareControlledInput;
