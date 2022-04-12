3dd4159710f366d68eaa214f87131ecd4536237d
Juanra GM <juanrgm724@gmail.com>
2022-04-13 00:33:29 +0200

    refactor(system): remove `classList` property already used by SolidJS

--- packages/system/src/Dynamic/Dynamic.tsx
+++ packages/system/src/Dynamic/Dynamic.tsx
@@ -1,15 +1,10 @@
-import createClassListEffect from "../createClassListEffect";
-import createElementRef from "../createElementRef";
 import defineComponent from "../defineComponent";
 import { DynamicTypeMap } from "./DynamicProps";
 import { Dynamic as DynamicBase } from "solid-js/web";
 
 export const Dynamic = defineComponent<DynamicTypeMap>(function Dynamic(props) {
   if (!props.component) throw new Error(`Component prop is empty`);
-  const element = createElementRef(props);
-
-  createClassListEffect(element, () => props.classList);
-  return <DynamicBase {...props} ref={element} />;
+  return <DynamicBase {...props} />;
 });
 
 export default Dynamic;
--- packages/system/src/Dynamic/DynamicProps.ts
+++ packages/system/src/Dynamic/DynamicProps.ts
@@ -1,11 +1,9 @@
-import { ClassListProp } from "../createClassListEffect";
 import { ElementType, OverrideProps } from "@suid/types";
 import { JSXElement } from "solid-js";
 
 export interface DynamicSelfProps {
   children?: JSXElement;
   component?: ElementType;
-  classList?: ClassListProp;
 }
 
 export interface DynamicTypeMap<P = {}, D extends ElementType = "div"> {
