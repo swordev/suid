cf32d32fcfb6fe2fd5946720f1bb501053d890c4
Juanra GM <juanrgm724@gmail.com>
2022-04-17 20:58:50 +0200

    feat(material): allow passing theme objects

---
+++ .changeset/dirty-bats-drive.md
@@ -0,0 +1,5 @@
+---
+"@suid/material": patch
+---
+
+Allow passing theme objects
--- packages/material/src/styles/createTheme.tsx
+++ packages/material/src/styles/createTheme.tsx
@@ -44,6 +44,30 @@ export interface ThemeOptions<T extends string = Breakpoint>
   zIndex: ZIndexOptions;
 }
 
+export interface ThemeInput<T extends string = Breakpoint> {
+  breakpoints:
+    | DeepPartial<ThemeOptions<T>["breakpoints"]>
+    | (() => Theme<T>["breakpoints"]);
+  shape: DeepPartial<ThemeOptions<T>["shape"]> | (() => Theme<T>["shape"]);
+  spacing:
+    | DeepPartial<ThemeOptions<T>["spacing"]>
+    | (() => Theme<T>["spacing"]);
+  components:
+    | DeepPartial<ThemeOptions<T>["components"]>
+    | (() => Theme<T>["components"]);
+  palette:
+    | DeepPartial<ThemeOptions<T>["palette"]>
+    | (() => Theme<T>["palette"]);
+  typography:
+    | DeepPartial<ThemeOptions<T>["typography"]>
+    | (() => Theme<T>["typography"]);
+  shadows: () => Theme<T>["shadows"];
+  transitions: () => Theme<T>["transitions"];
+  zIndex: DeepPartial<ThemeOptions<T>["zIndex"]> | (() => Theme<T>["zIndex"]);
+  direction: () => Theme<T>["direction"];
+  mixins: () => Theme<T>["mixins"];
+}
+
 export function createThemeOptions<T extends string>(
   options: Partial<ThemeOptions<T>>
 ) {
@@ -51,24 +75,42 @@ export function createThemeOptions<T extends string>(
 }
 
 export function createTheme<T extends string = Breakpoint>(
-  options?: DeepPartial<ThemeOptions<T>>
+  input: Partial<ThemeInput<T>> = {}
 ): Theme<T> {
-  const breakpoints = createBreakpoints(options?.breakpoints);
-  const result: Theme<T> = {
+  const theme: Theme<T> = {
     direction: "ltr",
-    ...options,
-    breakpoints,
-    components: createComponents(options?.components),
-    palette: createPalette(options?.palette),
-    shape: createShape(options?.shape),
-    spacing: createSpacing(options?.spacing as any),
-    typography: createTypography(options?.typography),
-    shadows: createShadows(),
-    transitions: createTransitions({}),
-    zIndex: createZIndex(options?.zIndex),
-    mixins: createMixins(breakpoints),
-  };
-  return result;
+    ...input,
+  } as any;
+
+  function def<K extends keyof Theme>(
+    key: K,
+    defaults: (input: Partial<ThemeOptions<T>>) => any
+  ) {
+    const inputValue = input[key];
+    Object.defineProperty(theme, key, {
+      configurable: true,
+      enumerable: true,
+      ...(typeof inputValue === "function"
+        ? {
+            get: inputValue as any,
+          }
+        : {
+            value: defaults({ [key]: inputValue }),
+          }),
+    });
+  }
+
+  def("breakpoints", (input) => createBreakpoints(input.breakpoints as any));
+  def("components", (input) => createComponents(input.components as any));
+  def("palette", (input) => createPalette(input.palette));
+  def("shape", (input) => createShape(input.shape));
+  def("spacing", (input) => createSpacing(input.spacing as any));
+  def("typography", (input) => createTypography(input.typography));
+  def("shadows", () => createShadows());
+  def("transitions", () => createTransitions({}));
+  def("zIndex", (input) => createZIndex(input.zIndex));
+  def("mixins", () => createMixins(theme.breakpoints));
+  return theme;
 }
 
 export function createThemeStore<T extends string = Breakpoint>(
