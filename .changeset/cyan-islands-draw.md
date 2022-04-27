8f2a158c8eeac1f25cd3c9cde3e3fe4952f8695d
Juanra GM <juanrgm724@gmail.com>
2022-04-27 21:47:59 +0200

    chore: update dependencies

---
+++ .changeset/cyan-islands-draw.md
@@ -0,0 +1,11 @@
+---
+"@suid/base": patch
+"@suid/codemod": patch
+"@suid/icons-material": patch
+"@suid/material": patch
+"@suid/site": patch
+"@suid/types": patch
+"@suid/utils": patch
+---
+
+Update dependencies
--- .ncurc.js
+++ .ncurc.js
@@ -1,4 +1,5 @@
 module.exports = {
   packageFile: ["./package.json", "./packages/*/package.json"],
-  reject: ["@suid/*"],
+  reject: ["@suid/*", "babel-jest", "jest"],
+  dep: "dev,optional,peer,prod,bundle",
 };
