00d7b31be79021acd5be5b8a03c24d1588fc7627
Juanra GM <juanrgm724@gmail.com>
2022-05-07 18:36:58 +0200

    feat(codemod): add `removeUnusedImports` transform

---
+++ packages/codemod/src/transforms/removeUnusedImports.ts
@@ -0,0 +1,38 @@
+import { SourceFile } from "ts-morph";
+
+export default function removeUnusedImports(source: SourceFile) {
+  const allImports = source.getImportDeclarations();
+
+  for (const importNode of allImports) {
+    const defaultImport = importNode.getDefaultImport();
+    const namedImports = importNode.getNamedImports();
+    const nsImport = importNode.getNamespaceImport();
+    let removed = 0;
+
+    if (defaultImport?.findReferencesAsNodes().length === 1) {
+      importNode.removeDefaultImport();
+      removed++;
+    }
+
+    for (const nameImport of namedImports) {
+      const node = nameImport.getAliasNode() || nameImport.getNameNode();
+      if (node.findReferencesAsNodes().length === 1) {
+        nameImport.remove();
+        removed++;
+      }
+    }
+    if (nsImport?.findReferencesAsNodes().length === 1) {
+      importNode.removeNamespaceImport();
+      removed++;
+    }
+
+    if (
+      removed &&
+      !importNode.getDefaultImport() &&
+      !importNode.getNamedImports().length &&
+      !importNode.getNamespaceImport()
+    ) {
+      importNode.remove();
+    }
+  }
+}
---
+++ packages/codemod/test/transforms/removeUnusedImports.test.ts
@@ -0,0 +1,90 @@
+import removeUnusedImports from "../../src/transforms/removeUnusedImports";
+import format from "../format";
+import transform from "../transform";
+
+const t = (code: string) => transform(code, [removeUnusedImports]);
+
+describe("removeUnusedImports", () => {
+  it("removes default import", () => {
+    expect(
+      t(`
+        import a from "a";
+        import b from "b";
+        console.log(a)
+      `)
+    ).toBe(
+      format(`
+        import a from "a";
+        console.log(a)
+      `)
+    );
+  });
+  it("removes namespace import", () => {
+    expect(
+      t(`
+        import * as a from "a";
+        import * as b from "b";
+        console.log(a)
+      `)
+    ).toBe(
+      format(`
+        import * as a from "a";
+        console.log(a)
+      `)
+    );
+  });
+  it("removes named import", () => {
+    expect(
+      t(`
+        import { a } from "a";
+        import { b } from "b";
+        console.log(a)
+      `)
+    ).toBe(
+      format(`
+        import { a } from "a";
+        console.log(a)
+      `)
+    );
+  });
+  it("removes named import with alias", () => {
+    expect(
+      t(`
+        import { a as x } from "a";
+        import { b } from "b";
+        console.log(x)
+      `)
+    ).toBe(
+      format(`
+        import { a as x } from "a";
+        console.log(x)
+      `)
+    );
+  });
+  it("removes named", () => {
+    expect(
+      t(`
+        import { a, x, y, z } from "a";
+        import { b } from "b";
+        console.log(a)
+      `)
+    ).toBe(
+      format(`
+        import { a } from "a";
+        console.log(a)
+      `)
+    );
+  });
+  it("keeps import without from", () => {
+    expect(
+      t(`
+        import "a";
+        import { b } from "b";
+      `)
+    ).toBe(
+      format(`
+        import "a";
+      `)
+    );
+  });
+});
