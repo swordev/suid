1aced166dea7fb9b5d3636048610284c79d6fc7f
Juanra GM <juanrgm724@gmail.com>
2022-05-21 17:23:35 +0200

    fix(system): add px unit suffix

---
+++ .changeset/tricky-ears-talk.md
@@ -0,0 +1,5 @@
+---
+"@suid/system": patch
+---
+
+Add px unit suffix when necessary
--- packages/system/src/resolveStyledProps.ts
+++ packages/system/src/resolveStyledProps.ts
@@ -2,7 +2,56 @@ import { resolvedPropKey } from "./createSxClass";
 import { StyledProps } from "./styledProps";
 import resolve from "@suid/css/resolve";
 
-export const unitLess = new Set<string>([]);
+/**
+ * @link https://github.com/emotion-js/emotion/blob/main/packages/unitless/src/index.js
+ */
+export const unitLess = new Set([
+  "animationIterationCount",
+  "borderImageOutset",
+  "borderImageSlice",
+  "borderImageWidth",
+  "boxFlex",
+  "boxFlexGroup",
+  "boxOrdinalGroup",
+  "columnCount",
+  "columns",
+  "flex",
+  "flexGrow",
+  "flexPositive",
+  "flexShrink",
+  "flexNegative",
+  "flexOrder",
+  "gridRow",
+  "gridRowEnd",
+  "gridRowSpan",
+  "gridRowStart",
+  "gridColumn",
+  "gridColumnEnd",
+  "gridColumnSpan",
+  "gridColumnStart",
+  "msGridRow",
+  "msGridRowSpan",
+  "msGridColumn",
+  "msGridColumnSpan",
+  "fontWeight",
+  "lineHeight",
+  "opacity",
+  "order",
+  "orphans",
+  "tabSize",
+  "widows",
+  "zIndex",
+  "zoom",
+  "WebkitLineClamp",
+  "fillOpacity",
+  "floodOpacity",
+  "stopOpacity",
+  "strokeDasharray",
+  "strokeDashoffset",
+  "strokeMiterlimit",
+  "strokeOpacity",
+  "strokeWidth",
+]);
 
 export function resolveStyledPropsValue(name: string, value: unknown) {
   if (typeof value === "number") {
