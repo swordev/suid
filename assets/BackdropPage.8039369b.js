377b49a34fea951dece54bd7d358aad6775c70fe
juanrgm <54169354+juanrgm@users.noreply.github.com>
2022-06-11 12:04:42 +0000

    Deploying to gh-pages from @ swordev/suid@e5830d40aef5dd1018d83e9bc826eefd260de1fd 🚀

---
+++ assets/BackdropPage.8039369b.js
@@ -0,0 +1,21 @@
+var m=Object.defineProperty;var t=(n,e)=>m(n,"name",{value:e,configurable:!0});import{u as c,a as l,ab as p,d as r,h as d,ac as s,t as u}from"./index.5728258e.js";import{C as f}from"./ComponentInfo.b4aab114.js";import"./Link.5c554c09.js";import"./copyText.3eadc357.js";import"./PaperCode.6e925dac.js";const k=u("<div></div>");function i(){const n=c(),[e,a]=l(!1);return(()=>{const o=k.cloneNode(!0);return p(o,r(d,{onClick:()=>a(!e()),children:"Show backdrop"}),null),p(o,r(s,{get sx(){return{color:"#fff",zIndex:n.zIndex.drawer+1}},get open(){return e()},onClick:()=>a(!1)}),null),o})()}t(i,"SimpleBackdrop");i.code=`import Backdrop from "@suid/material/Backdrop";
+import Button from "@suid/material/Button";
+import useTheme from "@suid/material/styles/useTheme";
+import { createSignal } from "solid-js";
+
+export default function SimpleBackdrop() {
+  const theme = useTheme();
+  const [open, setOpen] = createSignal(false);
+
+  return (
+    <div>
+      <Button onClick={() => setOpen(!open())}>Show backdrop</Button>
+      <Backdrop
+        sx={{ color: "#fff", zIndex: theme.zIndex.drawer + 1 }}
+        open={open()}
+        onClick={() => setOpen(false)}
+      ></Backdrop>
+    </div>
+  );
+}
+`;function I(){return r(f,{get name(){return s.name},examples:[{component:i,title:!1}]})}t(I,"BackdropPage");export{I as default};
