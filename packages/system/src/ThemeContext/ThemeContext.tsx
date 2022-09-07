c5f7ab08bfa4e2e274a0d8e191116f9172868082
Juanra GM <juanrgm724@gmail.com>
2022-09-07 19:15:25 +0200

    fix(system): preserve `ThemeContext` reference in dev mode

--- packages/system/src/ThemeContext/ThemeContext.tsx
+++ packages/system/src/ThemeContext/ThemeContext.tsx
@@ -1,5 +1,20 @@
-import { createContext } from "solid-js";
+import { Context, createContext } from "solid-js";
 
-const ThemeContext = createContext({});
+let ThemeContext!: Context<{}>;
+
+type Global = {
+  __suid?: {
+    systemThemeContext?: Context<{}>;
+  };
+};
+
+if (process.env.NODE_ENV !== "production") {
+  const global: Global = globalThis as any;
+  const suid = global.__suid || (global.__suid = {});
+  ThemeContext =
+    suid.systemThemeContext || (suid.systemThemeContext = createContext({}));
+} else {
+  ThemeContext = createContext({});
+}
 
 export default ThemeContext;
