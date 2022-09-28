0d2fc0dc75638a32dd8908f1eaaf7cde85b25527
kkocdko <31189892+kkocdko@users.noreply.github.com>
2022-09-28 18:21:59 +0800

    Remove redundant code
    Co-authored-by: Juanra GM <54169354+juanrgm@users.noreply.github.com>

--- packages/system/src/styleFunctionSx/index.ts
+++ packages/system/src/styleFunctionSx/index.ts
@@ -3,4 +3,4 @@ import { Breakpoint } from "../createTheme/createBreakpoints";
 export type ResponsiveStyleValue<T> =
   | T
   | Array<T | undefined>
-  | { [key in Breakpoint]?: T | undefined };
+  | { [key in Breakpoint]?: T };
