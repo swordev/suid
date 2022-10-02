705b1523437f2b32ec8892f02a7bbef4e58c17ac
Juanra GM <juanrgm724@gmail.com>
2022-10-02 20:40:45 +0200

    chore: update dependencies

---
+++ .changeset/clean-ravens-doubt.md
@@ -0,0 +1,9 @@
+---
+"@suid/codemod": patch
+"create-suid": patch
+"@suid/site": patch
+"@suid/system": patch
+"@suid/vite-plugin": patch
+---
+
+Update dependencies
--- packages/system/src/Dynamic/Dynamic.tsx
+++ packages/system/src/Dynamic/Dynamic.tsx
@@ -1,10 +1,17 @@
 import defineComponent from "../defineComponent";
 import { DynamicTypeMap } from "./DynamicProps";
+import { ElementType } from "@suid/types";
 import { Dynamic as DynamicBase } from "solid-js/web";
 
 export const Dynamic = defineComponent<DynamicTypeMap>(function Dynamic(props) {
   if (!props.component) throw new Error(`Component prop is empty`);
-  return <DynamicBase {...props} />;
+  return (
+    <DynamicBase
+      {...(props as typeof props & {
+        component: ElementType;
+      })}
+    />
+  );
 });
 
 export default Dynamic;
