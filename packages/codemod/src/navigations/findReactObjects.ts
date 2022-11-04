cc8fd18633bfd70bdca7bf2431e1c3e4df5de63f
Milan Hauth <milahu@gmail.com>
2022-11-04 16:42:26 +0100

    fix(codemod): handle multiple react imports

--- packages/codemod/src/navigations/findReactObjects.ts
+++ packages/codemod/src/navigations/findReactObjects.ts
@@ -2,17 +2,21 @@ import getParentExpr from "../utils/getParentExpr";
 import { Identifier, Node, SourceFile, ts } from "ts-morph";
 
 export default function findReactObjects(source: SourceFile, names?: string[]) {
-  const reactImport = source.getImportDeclaration("react");
+  const reactImports = (
+    source.getImportDeclarations()
+    .filter(d => d.getModuleSpecifier().getLiteralValue() == "react")
+  );
   const result: { name: string; node: Identifier }[] = [];
   const push = (ref: Identifier) => {
     const name = ref.getText();
     if (!names || names.includes(name))
+      // TODO(milahu): dedupe?
       result.push({
         name,
         node: ref,
       });
   };
-  if (reactImport) {
+  for (const reactImport of reactImports) {
     const ns = (
       reactImport.getNamespaceImport() || // import * as React from "react"
       reactImport.getDefaultImport() // import React from "react"
---
+++ packages/codemod/test/navigations/findReactObjects.test.ts
@@ -0,0 +1,63 @@
+import findReactObjects from "../../src/navigations/findReactObjects";
+import createProject from "../../src/utils/createProject";
+
+// based on src/utils/applyTransforms.ts
+
+const t = (input: string) => {
+  const project = createProject();
+  const sourceFile = project.createSourceFile("file.tsx", input, {
+    overwrite: true,
+  });
+  console.log("sourceFile", sourceFile);
+  const reactObjects = findReactObjects(sourceFile);
+  console.log("reactObjects", reactObjects);
+  return reactObjects.map(({ name, node }) => ({
+    name,
+    nodePos: node.getPos(),
+  }));
+};
+
+describe("findReactObjects", () => {
+  it("handles multiple react imports", () => {
+    expect(
+      t(`
+        import * as React from "react";
+        import { useState } from "react";
+        var x = React.useEffect
+        var x = useState
+      `)
+    ).toMatchInlineSnapshot(`
+      [
+        {
+          "name": "useEffect",
+          "nodePos": 105,
+        },
+        {
+          "name": "useState",
+          "nodePos": 130,
+        },
+      ]
+    `);
+  });
+  it("handles multiple react imports with duplicates", () => {
+    expect(
+      t(`
+        import * as React from "react";
+        import { useState } from "react";
+        var x = React.useState
+        var x = useState
+      `)
+    ).toMatchInlineSnapshot(`
+      [
+        {
+          "name": "useState",
+          "nodePos": 105,
+        },
+        {
+          "name": "useState",
+          "nodePos": 129,
+        },
+      ]
+    `);
+  });
+});
