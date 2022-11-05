30094db455e194893f415986be0517887eee8e1c
Milan Hauth <milahu@gmail.com>
2022-11-05 18:16:04 +0100

    todo(codemod): fix type imports

--- packages/codemod/src/transforms/replaceReactCSSPropertiesType.ts
+++ packages/codemod/src/transforms/replaceReactCSSPropertiesType.ts
@@ -8,6 +8,9 @@ export default function replaceReactElementType(node: Identifier) {
   node.getSourceFile().addImportDeclaration({
     moduleSpecifier: "csstype",
     namespaceImport: "CSS",
+    // FIXME(milahu): import type
+    // a: import * as CSS from "csstype";
+    // b: import type * as CSS from "csstype";
     isTypeOnly: true,
   });
 }
