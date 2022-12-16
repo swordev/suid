b36ed9b90ee0087e04c0866bd7f5bab8e9ef66a4
Juanra GM <juanrgm724@gmail.com>
2022-12-16 19:49:59 +0100

    feat(material): export `styled` function

--- packages/material/src/styles/index.tsx
+++ packages/material/src/styles/index.tsx
@@ -16,5 +16,6 @@ export type { Breakpoint } from "@suid/system";
 export { default as useTheme } from "./useTheme";
 export { default as useThemeProps } from "./useThemeProps";
 export { default as ThemeProvider } from "./ThemeProvider";
+export { default as styled } from "./styled";
 export { createTheme } from "./createTheme";
 export type { Theme } from "./createTheme";
