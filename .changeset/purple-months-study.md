fa8d6eb7fb84507f52067c8224b991125f4d02d7
Juanra GM <juanrgm724@gmail.com>
2023-03-21 01:37:56 +0100

    feat(css): find style element from the host element

---
+++ .changeset/purple-months-study.md
@@ -0,0 +1,5 @@
+---
+"@suid/css": minor
+---
+
+Allow find the style element from the host element
--- packages/css/src/dom/findStyleElement.ts
+++ packages/css/src/dom/findStyleElement.ts
@@ -1,5 +1,21 @@
-function findStyleElement(id: string) {
-  return document.getElementById(id) as HTMLStyleElement | undefined;
+function findStyleElement(
+  element: Element,
+  classPrefix?: string
+): HTMLStyleElement | undefined;
+function findStyleElement(id: string): HTMLStyleElement | undefined;
+function findStyleElement(
+  input: string | Element,
+  classPrefix = "css-"
+): HTMLStyleElement | undefined {
+  if (typeof input === "string")
+    return document.getElementById(input) as HTMLStyleElement | undefined;
+  const className = [...input.classList].find((name) =>
+    name.startsWith(classPrefix)
+  );
+  if (className)
+    return document.getElementById(className.slice(classPrefix.length)) as
+      | HTMLStyleElement
+      | undefined;
 }
 
 export default findStyleElement;
