3ed4dd272a12dab771cbaca1e27fbb8cdbd28863
Juanra GM <juanrgm724@gmail.com>
2023-03-31 18:33:48 +0200

    chore: disable eslint rule

--- packages/material/src/Select/SelectInput.tsx
+++ packages/material/src/Select/SelectInput.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { isFilled } from "../InputBase/utils";
 import Menu from "../Menu/Menu";
 import MenuItem, { MenuItemTypeMap } from "../MenuItem";
