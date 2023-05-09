6067207bb6ef1e0dcbed67915de9d36f9f4e87a2
Juanra GM <juanrgm724@gmail.com>
2023-05-09 20:38:30 +0200

    chore: lock to minor version

--- .ncurc.js
+++ .ncurc.js
@@ -1,5 +1,14 @@
+// @ts-check
+/** @type {import("npm-check-updates").RunOptions} */
 module.exports = {
   workspaces: true,
   root: true,
   dep: "dev,optional,prod,bundle,peer",
+  target: (name) => {
+    if (["ts-morph", "commander", "@types/node"].includes(name)) {
+      return "minor";
+    } else {
+      return "latest";
+    }
+  },
 };
