ba6335938ef552f146fc68f1325822190ce96298
Juanra GM <juanrgm724@gmail.com>
2023-05-09 20:42:30 +0200

    chore: update dependencies

--- .ncurc.js
+++ .ncurc.js
@@ -1,9 +1,8 @@
-// @ts-check
 /** @type {import("npm-check-updates").RunOptions} */
 module.exports = {
   workspaces: true,
   root: true,
-  dep: "dev,optional,prod,bundle,peer",
+  dep: "dev,optional,prod,bundle",
   target: (name) => {
     if (["ts-morph", "commander", "@types/node"].includes(name)) {
       return "minor";
