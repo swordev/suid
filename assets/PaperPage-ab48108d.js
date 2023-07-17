8e5dd4d64d48adfed7329ba8ebbd8c53386143b8
juanrgm <54169354+juanrgm@users.noreply.github.com>
2023-07-17 22:31:07 +0000

    Deploying to gh-pages-next from @ swordev/suid@3b5a352950aee522fa719f5a871559fc534a7c9f 🚀

---
+++ assets/PaperPage-ab48108d.js
@@ -0,0 +1,22 @@
+var p=Object.defineProperty;var t=(a,o)=>p(a,"name",{value:o,configurable:!0});import{d as e,P as n,B as i}from"./index-58d67e0c.js";import{C as l}from"./ComponentInfo-7a5efda9.js";import"./Link-a6b64480.js";import"./copyText-a21b3bd2.js";import"./PaperCode-e8308b2e.js";function r(){return e(i,{sx:{display:"flex",flexWrap:"wrap","& > :not(style)":{m:1,width:128,height:128}},get children(){return[e(n,{elevation:0}),e(n,{}),e(n,{elevation:3})]}})}t(r,"BasicPaper");r.code=`import { Box, Paper } from "@suid/material";
+
+export default function BasicPaper() {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexWrap: "wrap",
+        "& > :not(style)": {
+          m: 1,
+          width: 128,
+          height: 128,
+        },
+      }}
+    >
+      <Paper elevation={0} />
+      <Paper />
+      <Paper elevation={3} />
+    </Box>
+  );
+}
+`;function u(){return e(l,{get name(){return n.name},examples:[{bgcolor:"contrasted",component:r}]})}t(u,"PaperPage");export{u as default};
