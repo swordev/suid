cd68d561280f546e0b867e94c53a601a8ecb275f
juanrgm <54169354+juanrgm@users.noreply.github.com>
2023-07-25 07:34:06 +0000

    Deploying to gh-pages from @ swordev/suid@4b847b68badc045fb3be2eaaf1c3137411c5808a 🚀

---
+++ assets/BoxPage-d149b3da.js
@@ -0,0 +1,20 @@
+var m=Object.defineProperty;var o=(e,a)=>m(e,"name",{value:a,configurable:!0});import{i,d as t,B as n}from"./index-d00df503.js";import{C as p}from"./ComponentInfo-39582af4.js";import"./Link-dabd3986.js";import"./copyText-a8be6e54.js";import"./PaperCode-95b8e717.js";function r(){const e=i();return t(n,{get sx(){return{width:300,height:300,backgroundColor:e.palette.primary.dark,"&:hover":{backgroundColor:e.palette.primary.main,opacity:.9}}}})}o(r,"BoxSx");r.code=`import { Box } from "@suid/material";
+import useTheme from "@suid/material/styles/useTheme";
+
+export default function BoxSx() {
+  const theme = useTheme();
+  return (
+    <Box
+      sx={{
+        width: 300,
+        height: 300,
+        backgroundColor: theme.palette.primary.dark,
+        "&:hover": {
+          backgroundColor: theme.palette.primary.main,
+          opacity: 0.9,
+        },
+      }}
+    />
+  );
+}
+`;function x(){return t(p,{get name(){return n.name},docsApiName:"components/box",moreExamples:!1,examples:[{component:r,title:!1}]})}o(x,"BoxPage");export{x as default};
