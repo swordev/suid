8d7752d8901eb6c3710a911c89c54a430fb4bc41
David Bieregger <david-e@outlook.de>
2023-08-21 00:58:58 +0200

    Add LoadingButton

---
+++ packages/lab/src/LoadingButton/index.tsx
@@ -0,0 +1,7 @@
+export { default } from "./LoadingButton";
+export * from "./LoadingButton";
+
+export { default as loadingButtonClasses } from "./loadingButtonClasses";
+export * from "./loadingButtonClasses";
+
+export * from "./LoadingButtonProps";
