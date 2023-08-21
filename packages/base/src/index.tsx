9193259ebfd7e0065a9b4af9d5f122e59e58ca76
David Bieregger <david-e@outlook.de>
2023-08-21 10:41:44 +0200

    Export useTheme, so that the imports look more beautiful

--- packages/base/src/index.tsx
+++ packages/base/src/index.tsx
@@ -4,3 +4,4 @@ import generateUtilityClasses from "./generateUtilityClasses";
 export { generateUtilityClasses };
 export { generateUtilityClass };
 export { default as unstable_composeClasses } from "./composeClasses";
+export { default as useTheme } from "./useTheme";
