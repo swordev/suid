13eeda952f89970acc59a23d7757617c42b12d89
David Bieregger <david-e@outlook.de>
2023-08-21 11:48:17 +0200

    Revert "Export useTheme, so that the imports look more beautiful"
    This reverts commit 9193259ebfd7e0065a9b4af9d5f122e59e58ca76.

--- packages/base/src/index.tsx
+++ packages/base/src/index.tsx
@@ -4,4 +4,3 @@ import generateUtilityClasses from "./generateUtilityClasses";
 export { generateUtilityClasses };
 export { generateUtilityClass };
 export { default as unstable_composeClasses } from "./composeClasses";
-export { default as useTheme } from "./useTheme";
