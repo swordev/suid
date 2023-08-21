533180cf3261af043aa8651d91a2c6ad90ddfabd
Juanra GM <juanrgm724@gmail.com>
2023-08-21 18:51:04 +0200

    refactor(site): rename components

--- packages/site/src/pages/customization/theme/ComponentsPage/ComponentsPage.tsx
+++ packages/site/src/pages/customization/theme/ComponentsPage/ComponentsPage.tsx
@@ -1,7 +1,7 @@
 import ComponentInfo from "~/components/ComponentInfo";
 import DefaultPropsExample from "./DefaultPropsExample";
 
-export default function StyledPage() {
+export default function ComponentsPage() {
   return (
     <ComponentInfo
       name="Components"
--- packages/site/src/pages/customization/theme/PalettePage/PalettePage.tsx
+++ packages/site/src/pages/customization/theme/PalettePage/PalettePage.tsx
@@ -2,7 +2,7 @@ import ComponentInfo from "~/components/ComponentInfo";
 import CustomColorExample from "./CustomColorExample";
 import Example from "./Example";
 
-export default function StyledPage() {
+export default function PalettePage() {
   return (
     <ComponentInfo
       name="Palette"
