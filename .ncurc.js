5abb43c65e70455ad6f06a4ac296a155f8c83e25
Juanra GM <juanrgm724@gmail.com>
2023-09-24 18:33:22 +0200

    chore: lock dependency versions

--- .ncurc.js
+++ .ncurc.js
@@ -1,13 +1,30 @@
 /** @type {import("npm-check-updates").RunOptions} */
+
+const targets = {
+  patch: [
+    // https://github.com/jsdom/jsdom/issues/3562
+    "jsdom",
+  ],
+  minor: [
+    // pending (breaking changes)
+    // https://github.com/vitest-dev/vitest/releases/tag/v0.34.0
+    "vitest",
+    // pending
+    "ts-morph",
+    // CJS dropped
+    "commander",
+    // NodeJS v18
+    "@types/node",
+  ],
+};
+
 module.exports = {
   workspaces: true,
   root: true,
   dep: "dev,optional,prod,bundle,peer",
   target: (name) => {
-    if (["ts-morph", "commander", "@types/node"].includes(name)) {
-      return "minor";
-    } else {
-      return "latest";
-    }
+    for (const target in targets)
+      if (targets[target].includes(name)) return target;
+    return "latest";
   },
 };
