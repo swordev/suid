e05489f267df72c02927af3903c74cd0d9c650f7
juanrgm <54169354+juanrgm@users.noreply.github.com>
2023-09-24 17:48:12 +0000

    Deploying to gh-pages-next from @ swordev/suid@0e4f086f198d78f67e0bc274ed125181f1e86fa9 🚀

--- assets/useFormControl-e2d3f851.js
+++
@@ -1,1 +0,0 @@
-var e=Object.defineProperty;var o=(t,r)=>e(t,"name",{value:r,configurable:!0});import{a2 as n,a3 as a}from"./index-dcf73f67.js";const s=n();function m(){return a(s)}o(m,"useFormControl");export{s as F,m as u};
--- assets/useMediaQueryPage-03738525.js
+++
@@ -1,8 +0,0 @@
-var r=Object.defineProperty;var n=(t,e)=>r(t,"name",{value:e,configurable:!0});import{j as a,E as i,t as s,h as o}from"./index-dcf73f67.js";import{C as p}from"./ComponentInfo-b324653a.js";import"./Link-1d61b786.js";import"./copyText-676cc977.js";import"./PaperCode-2aa6fb01.js";const u=s("<span>");function m(){const t=a("(min-width:600px)");return(()=>{const e=u();return i(e,()=>`(min-width:600px) matches: ${t()}`),e})()}n(m,"SimpleMediaQuery");m.code=`import { useMediaQuery } from "@suid/material";
-
-export default function SimpleMediaQuery() {
-  const matches = useMediaQuery("(min-width:600px)");
-
-  return <span>{\`(min-width:600px) matches: \${matches()}\`}</span>;
-}
-`;function y(){return o(p,{get name(){return a.name},docsApiName:"components/use-media-query",examples:[m],moreExamples:!1})}n(y,"useMediaQueryPage");export{y as default};
