6a10e1de52422cacc5009b57a748561c60fa71d6
Juanra GM <juanrgm724@gmail.com>
2024-03-05 22:47:56 +0100

    chore: update prettier

--- packages/codemod/test/format.ts
+++ packages/codemod/test/format.ts
@@ -1,11 +1,13 @@
 import { format as _format } from "prettier";
 import parserTypeScript from "prettier/parser-typescript.js";
+import estree from "prettier/plugins/estree.js";
 
 export default async function format(code: string) {
   return (
     await _format(code, {
       filepath: "file.tsx",
-      plugins: [parserTypeScript],
+      parser: "typescript",
+      plugins: [parserTypeScript, estree],
     })
   )
     .split(/[\n]+/)
