6c77575f27727735650ecc81a1ed0697e6ae7201
Juanra GM <juanrgm724@gmail.com>
2024-03-19 20:47:29 +0100

    fix(material): export `Portal` component

--- packages/material/src/index.tsx
+++ packages/material/src/index.tsx
@@ -72,6 +72,7 @@ export { default as OutlinedInput } from "./OutlinedInput";
 export { default as Paper } from "./Paper";
 export { default as Popover } from "./Popover";
 export { default as Popper } from "./Popper";
+export { default as Portal } from "./Portal";
 export { default as Radio } from "./Radio";
 export { default as RadioGroup } from "./RadioGroup";
 export { default as Select } from "./Select";
