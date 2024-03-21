9492943fb8d5aba36f2a0ddc23c24f5e736375a1
juanrgm <54169354+juanrgm@users.noreply.github.com>
2024-03-21 20:33:08 +0000

    Deploying to gh-pages-next from @ swordev/suid@4a18c973976a1eba53bd55e3aeded82eaaa0251b 🚀

---
+++ assets/javascript-CyX5lvNz.js
@@ -0,0 +1,6 @@
+import{conf as t,language as e}from"./typescript-_bk7wFsb.js";import"./CodeEditor-HUyy_HK2.js";import"./index-D9l_Mn70.js";import"./useControlled-DOSjX3gy.js";/*!-----------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Version: 0.47.0(69991d66135e4a1fc1cf0b1ac4ad25d429866a0d)
+ * Released under the MIT license
+ * https://github.com/microsoft/monaco-editor/blob/main/LICENSE.txt
+ *-----------------------------------------------------------------------------*/var a=t,n={defaultToken:"invalid",tokenPostfix:".js",keywords:["break","case","catch","class","continue","const","constructor","debugger","default","delete","do","else","export","extends","false","finally","for","from","function","get","if","import","in","instanceof","let","new","null","return","set","static","super","switch","symbol","this","throw","true","try","typeof","undefined","var","void","while","with","yield","async","await","of"],typeKeywords:[],operators:e.operators,symbols:e.symbols,escapes:e.escapes,digits:e.digits,octaldigits:e.octaldigits,binarydigits:e.binarydigits,hexdigits:e.hexdigits,regexpctl:e.regexpctl,regexpesc:e.regexpesc,tokenizer:e.tokenizer};export{a as conf,n as language};
