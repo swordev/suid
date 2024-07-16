1c3c57eef88fcbb0e67b17b98b37b0fd67d553fc
juanrgm <54169354+juanrgm@users.noreply.github.com>
2024-07-16 16:27:18 +0000

    Deploying to gh-pages-next from @ swordev/suid@39bbc212efd0d55847a9cc1b3b7c8e7f6e685bf9 🚀

---
+++ assets/AppBarPage-CKWAKwlx.js
@@ -0,0 +1,34 @@
+var i=Object.defineProperty;var o=(e,a)=>i(e,"name",{value:a,configurable:!0});import{h as n,C as r,D as p,I as s,M as c,k as l,z as u,B as m}from"./index-DDK7TJDP.js";import{C as B}from"./ComponentInfo-D6rsRCnx.js";import"./Link-CQanOrc_.js";import"./copyText-DSIayLkX.js";import"./PaperCode-DVdegaHe.js";function t(){return n(m,{sx:{flexGrow:1},get children(){return n(r,{position:"static",get children(){return n(p,{get children(){return[n(s,{size:"large",edge:"start",color:"inherit","aria-label":"menu",sx:{mr:2},get children(){return n(c,{})}}),n(l,{variant:"h6",component:"div",sx:{flexGrow:1},children:"News"}),n(u,{color:"inherit",children:"Login"})]}})}})}})}o(t,"BasicAppBar");t.code=`import MenuIcon from "@suid/icons-material/Menu";
+import {
+  AppBar,
+  Box,
+  Button,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@suid/material";
+
+export default function BasicAppBar() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
+`;function y(){return n(B,{get name(){return r.name},examples:[{bgcolor:"contrasted",component:t}]})}o(y,"AppBarPage");export{y as default};
---
+++ assets/ContainerPage-C4dhlKHv.js
@@ -0,0 +1,13 @@
+var i=Object.defineProperty;var e=(r,a)=>i(r,"name",{value:a,configurable:!0});import{h as n,H as m,B as s,J as o}from"./index-DDK7TJDP.js";import{C as p}from"./ComponentInfo-D6rsRCnx.js";import"./Link-CQanOrc_.js";import"./copyText-DSIayLkX.js";import"./PaperCode-DVdegaHe.js";function t(){return[n(m,{}),n(o,{maxWidth:"sm",get children(){return n(s,{sx:{bgcolor:"#cfe8fc",height:"400px"}})}})]}e(t,"SimpleContainer");t.code=`import { Box, Container, CssBaseline } from "@suid/material";
+
+export default function SimpleContainer() {
+  return (
+    <>
+      <CssBaseline />
+      <Container maxWidth="sm">
+        <Box sx={{ bgcolor: "#cfe8fc", height: "400px" }} />
+      </Container>
+    </>
+  );
+}
+`;function d(){return n(p,{get name(){return o.name},examples:[{bgcolor:"contrasted",component:t}]})}e(d,"ContainerPage");export{d as default};
