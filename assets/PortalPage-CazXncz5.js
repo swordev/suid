83a907cb56ed1028084c69f7df90d97cc0a62a73
juanrgm <54169354+juanrgm@users.noreply.github.com>
2024-08-20 18:25:17 +0000

    Deploying to gh-pages from @ swordev/suid@6a28394b4e9f5965081f976470981bb3f2832e38 🚀

---
+++ assets/PortalPage-CazXncz5.js
@@ -0,0 +1,30 @@
+var f=Object.defineProperty;var n=(e,o)=>f(e,"name",{value:o,configurable:!0});import{h as t,at as h,au as x,a as b,E as l,b as s,B as c,t as m,ac as k}from"./index-ridAuENd.js";import{C as w}from"./ComponentInfo-CHqyrOGo.js";import"./Link-q_lmqSfc.js";import"./copyText-CCrSkyLZ.js";import"./PaperCode-Ae8UmCps.js";function u(e){return t(h,e)}n(u,"Portal");var g=m("<div><button type=button>"),y=m("<span>But I actually render here!");function d(){const[e,o]=b(!1),a=k(),p=n(()=>{o(!e())},"handleClick");return(()=>{var r=g(),i=r.firstChild;return i.$$click=p,l(i,()=>e()?"Unmount children":"Mount children"),l(r,t(c,{sx:{p:1,my:1,border:"1px solid"},get children(){return["It looks like I will render here.",s(()=>s(()=>!!e())()?t(u,{get container(){return a.ref},get children(){return y()}}):null)]}}),null),l(r,t(c,{sx:{p:1,my:1,border:"1px solid"},ref:a}),null),r})()}n(d,"SimplePortal");d.code=`import { Box, Portal } from "@suid/material";
+import createElementRef from "@suid/system/createElementRef";
+import { createSignal } from "solid-js";
+
+export default function SimplePortal() {
+  const [show, setShow] = createSignal(false);
+  const container = createElementRef();
+
+  const handleClick = () => {
+    setShow(!show());
+  };
+
+  return (
+    <div>
+      <button type="button" onClick={handleClick}>
+        {show() ? "Unmount children" : "Mount children"}
+      </button>
+      <Box sx={{ p: 1, my: 1, border: "1px solid" }}>
+        It looks like I will render here.
+        {show() ? (
+          <Portal container={container.ref}>
+            <span>But I actually render here!</span>
+          </Portal>
+        ) : null}
+      </Box>
+      <Box sx={{ p: 1, my: 1, border: "1px solid" }} ref={container} />
+    </div>
+  );
+}
+`;x(["click"]);function v(){return t(w,{get name(){return u.name},examples:[d]})}n(v,"PortalPage");export{v as default};
