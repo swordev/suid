a86d9c7aafbe8d395656b0361867ee55403d0db1
juanrgm <54169354+juanrgm@users.noreply.github.com>
2024-09-17 18:27:04 +0000

    Deploying to gh-pages-next from @ swordev/suid@0ae8bd26800c2ef53c8635d56134003877700152 🚀

---
+++ assets/SkeletonPage-BgMtkLNX.js
@@ -0,0 +1,12 @@
+var o=Object.defineProperty;var e=(r,i)=>o(r,"name",{value:i,configurable:!0});import{h as t,y as n}from"./index-QA7pP0xH.js";import{C as c}from"./ComponentInfo-Cp9In596.js";import{S as m}from"./copyText-DYDM2w-T.js";import"./Link-BRU1W-2I.js";import"./PaperCode-2wa1Y67L.js";function a(){return t(m,{spacing:1,get children(){return[t(n,{variant:"text"}),t(n,{variant:"circular",width:40,height:40}),t(n,{variant:"rectangular",width:210,height:118})]}})}e(a,"Variants");a.code=`import { Skeleton, Stack } from "@suid/material";
+
+export default function Variants() {
+  return (
+    <Stack spacing={1}>
+      <Skeleton variant="text" />
+      <Skeleton variant="circular" width={40} height={40} />
+      <Skeleton variant="rectangular" width={210} height={118} />
+    </Stack>
+  );
+}
+`;function S(){return t(c,{get name(){return n.name},examples:[a]})}e(S,"SkeletonPage");export{S as default};
